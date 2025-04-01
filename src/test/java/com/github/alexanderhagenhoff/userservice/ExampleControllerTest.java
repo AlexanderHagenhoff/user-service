ea027a058a82364c4b5e1e5b0ead6cc20a4798ee
AlexanderHagenhoff <hagenhoff.alexander@gmail.com>
2025-04-01 14:55:43 +0200

    u-003: Use java 23 to be compatible with jacoco-plugin. Add testcontainers for postgres. Add testcontainers configuration to run integration tests again.

--- src/test/java/com/github/alexanderhagenhoff/userservice/ExampleControllerTest.java
+++ src/test/java/com/github/alexanderhagenhoff/userservice/ExampleControllerTest.java
@@ -1,9 +1,10 @@
-package com.github.alexanderhagenhoff;
+package com.github.alexanderhagenhoff.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("integrationtest")
 public class ExampleControllerTest {
 
     private static final String EXPECTED_RESPONSE = "Hello from Spring Boot!";
