98029e6af67bdb96bc2b55197c70f0a414e08818
AlexanderHagenhoff <hagenhoff.alexander@gmail.com>
2025-04-09 17:27:15 +0200

    u-010: Use constant for oauth token valid time

--- src/main/java/com/github/alexanderhagenhoff/userservice/configuration/AppConfig.java
+++ src/main/java/com/github/alexanderhagenhoff/userservice/configuration/AppConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
