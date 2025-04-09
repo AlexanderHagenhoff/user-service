32d56956a304090caff6434b24a9b7c546fbb336
AlexanderHagenhoff <hagenhoff.alexander@gmail.com>
2025-04-09 18:07:39 +0200

    u-010: Deny all requests except oauth2

--- src/main/java/com/github/alexanderhagenhoff/userservice/configuration/SecurityConfig.java
+++ src/main/java/com/github/alexanderhagenhoff/userservice/configuration/SecurityConfig.java
@@ -5,15 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private static final String DEFAULT_CONTENT_SECURITY_POLICY = "default-src 'self'";
 
     @Bean
     @Order(1)
@@ -27,7 +35,20 @@ public class SecurityConfig {
     @Bean
     @Order(2)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().denyAll());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().denyAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(STATELESS))
+                .headers(headers -> headers
+                        .contentSecurityPolicy(csp -> csp
+                                .policyDirectives(DEFAULT_CONTENT_SECURITY_POLICY))
+                        .frameOptions(FrameOptionsConfig::sameOrigin))
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(UNAUTHORIZED))
+                );
+
         return http.build();
     }
 
